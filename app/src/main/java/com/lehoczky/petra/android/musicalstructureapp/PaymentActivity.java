09e5b95b86e479dfd6d9fdfffb8e218fe4e50e0c
mesibaja <petra.lehoczky@gmail.com>
2017-07-21 11:04:40 +0200

    Payment option added

---
+++ app/src/main/java/com/lehoczky/petra/android/musicalstructureapp/PaymentActivity.java
@@ -0,0 +1,10 @@
+package com.lehoczky.petra.android.musicalstructureapp;
+
+//I would create a public class for Constants, as seen below in the example from the tutorial: https://developers.google.com/android-pay/tutorial
+//public class Constants {
+// public static final int WALLET_ENVIRONMENT = WalletConstants.ENVIRONMENT_TEST;
+// public static final String MERCHANT_NAME = "Awesome Bike Store";
+// public static final String CURRENCY_CODE_USD = "USD";
+// }
+//public class PaymentActivity {
+//}
